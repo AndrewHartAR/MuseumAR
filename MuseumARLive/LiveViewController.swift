edd1aec7f2d100d81c6e1aa01aadc8c848cc8eb2
Andrew Hart <Andrew@ProjectDent.com>
2019-05-23 12:34:11 +0100

    adds liveVC target, for usage in a museum

---
+++ MuseumARLive/LiveViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MuseumARLive
+//
+//  Created by Andrew Hart on 23/05/2019.
+//  Copyright © 2019 Dent Reality. All rights reserved.
+//
+
+import UIKit
+
+class LiveViewController: UIViewController {
+	let artView = ARArtView()
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.addSubview(artView)
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+		artView.run()
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+		
+		// Pause the view's session
+		artView.pause()
+	}
+	
+	override func viewDidLayoutSubviews() {
+		super.viewDidLayoutSubviews()
+		
+		artView.frame = view.bounds
+	}
+}
+
